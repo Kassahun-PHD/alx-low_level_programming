8de6a5ea93fef3376dcc701caaa5894788865bd2
Kassahun-PHD <kassahunberisha@gmail.com>
2022-09-12 06:09:55 -0700

    a C program that prints the size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - the start of main function
  *
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - the start of main function
+ *
+ * Return: returns 0 at the end
+ */
+int main(void)
+{
+	return(0);
+}
