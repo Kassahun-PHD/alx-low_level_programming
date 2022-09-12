d4866cbe9837b33d22e6d709830b88d770595e7e
Kassahun-PHD <kassahunberisha@gmail.com>
2022-09-12 06:47:34 -0700

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error without using lists under man

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,9 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	
-	fwrite(str , 1 , sizeof(str) , stdout );
+
+	fwrite(str, 1, sizeof(str), stdout);
 	fflush(stdout);
 	fclose(stdout);
-	
 	return (1);
 }
