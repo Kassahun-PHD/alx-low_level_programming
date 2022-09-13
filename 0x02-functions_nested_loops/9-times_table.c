305a6e11dfdf0e6d54544800a2aab46ba50b4c4d
Kassahun-PHD <kassahunberisha@gmail.com>
2022-09-13 16:15:13 -0700

    times table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * times_table - prints times_table of 9
+ * Description: function prints times_table of 9
+ * Return: nothing
+ */
+void times_table(void);
+{
+	int r, c, p;
+
+	for (r = 0; r <= 9 ; r++)
+	{
+		for (c = 0; c < 9; c++)
+		{
+			p = r * c;
+			_putchar(p + '0');
+			_putchar(' ');
+		}
+		 _pitchar('\n');
+	}
+}
