17e37c99194672b065bda54a024130d0136a1bac
Kassahun-PHD <kassahunberisha@gmail.com>
2022-09-13 17:55:04 -0700

    addition of a two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,10 +3,12 @@
 
 /**
  * add - prints addition of a numbers
+ * @i: the first number
+ * @j: the second number
  * Description: function prints the sum of two numbers
  * Return: result
  */
 int add(int i, int j)
 {
-	return(i + j);
+	return (i + j);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,9 +15,13 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			n = i * j;
-			if (n <= 9)
+
+			if ((n / 10) == 0)
 			{
+				if (j != 0)
+					_putchar(' ');
 				_putchar(n + '0');
+
 				if (j == 9)
 					continue;
 				_putchar(',');
