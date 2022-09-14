79ba06b9cb5baf3d7231273234723bc7782c7bc8
Kassahun-PHD <kassahunberisha@gmail.com>
2022-09-13 17:44:40 -0700

    addition of a two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - prints addition of a numbers
+ * Description: function prints the sum of two numbers
+ * Return: result
+ */
+int add(int i, int j)
+{
+	return(i + j);
+}
