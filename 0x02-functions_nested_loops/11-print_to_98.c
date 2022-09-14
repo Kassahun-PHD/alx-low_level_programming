27b0abe922529b4ee10f9c129a8b216c3aea1886
Kassahun-PHD <kassahunberisha@gmail.com>
2022-09-14 04:16:29 -0700

    a function that prints all natural numbers from n to 98, followed by a new line.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,33 +9,43 @@
  */
 void print_to_98(int n)
 {
+	int i;
 
-        int i;
-
-        if (n < 98)
-        {
-                for (i = n; i <= 98; i++)
-                {
-                        _putchar((i / 10) + '0');
-                        _putchar((i % 10) + '0');
-                        if (i == 98)
-                                continue;
-                        _putchar(',');
-                        _putchar(' ');
-                }
-                _putchar('\n');
-        }
-        else
-        {
-                for (i = n; i >= 98; i--)
-                {
-                        _putchar((i / 10) + '0');
-                        _putchar((i % 10) + '0');
-                        if (i == 98)
-                                continue;
-                        _putchar(',');
-                        _putchar(' ');
-                }
-                _putchar('\n');
-        }
+	if (n > 0 && n <= 98)
+	{
+		if (i < 10)
+			for (i = n; i <= 9; i++)
+			{
+				_putchar((i % 10) + '0');
+				if (i == 98)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		else
+			for (i = 10; i <= 98; i++)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+				if (i == 98)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		_putchar('\n');
+	}
+	else if (n > 98 && n <= 111)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar((i / 100) + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			if (i == 98)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
 }
