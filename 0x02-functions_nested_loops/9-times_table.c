64548230de7f7463ca7d248a994bff0bfaa51eb4
Kassahun-PHD <kassahunberisha@gmail.com>
2022-09-13 17:13:34 -0700

    times table of 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,12 +14,13 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar(',');
-			_putchar(' ');
 			n = i * j;
-			
 			if (n <= 9)
+			{
+				_putchar(n + '0');
+				_putchar(',');
 				_putchar(' ');
+			}
 			else
 				_putchar((n / 10) + '0');
 			_putchar((n % 10) + '0');
