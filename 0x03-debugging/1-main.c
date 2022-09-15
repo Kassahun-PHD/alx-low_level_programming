e20a974da764aaf61cb5cf7fa3def80ad9234691
Kassahun-PHD <kassahunberisha@gmail.com>
2022-09-15 15:11:09 -0700

    program prints the largest of three integers

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,9 @@ int main(void)
 
         i = 0;
 
-        while (i < 10)
+        while (i < 10)   /* Here the loop goes to infinite as i does not incrimenting*/
         {
-                putchar(i);/* Here the loop goes to infinite as i does not incrimenting*/
+                putchar(i);
         }
 
         printf("Infinite loop avoided! \\o/\n");
