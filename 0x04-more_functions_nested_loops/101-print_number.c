51325dbcc3053c198b2677c1ae535cc1f9605f31
Kassahun-PHD <kassahunberisha@gmail.com>
2022-09-16 03:06:11 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,11 +6,28 @@
  */
 void print_integer(int m)
 {
-	int i = 10000;
+	int i = 1000000000;
 
+	if (m > 0)
+	{
 	for (; i >= 1; i /= 10)
 		if (m / i != 0)
 		{
 			_putchar((m / i) % 10 + '0');
 		}
+	}
+	if (m == 0)
+	{
+		_putchar(m + '0');
+	}
+	else
+	{
+		m = m * -1;
+		_putchar('-');
+		for (; i >= 1; i /= 10)
+			if (m / i != 0)
+			{
+				_putchar((m / i) % 10 + '0');
+			}
+	}
 }
